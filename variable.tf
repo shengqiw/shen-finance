f285e1eb2348d8cb98a7863775e012a292b844c4
Shengqi Wang <shengqiw97@gmail.com>
2023-07-25 16:09:30 -0500

    test3

--- variable.tf
+++ variable.tf
@@ -1,6 +1,8 @@
 variable "access_key" {
         description = "Access key to AWS console"
+        default = "AKIAVXVHRLCE5VOPFEGQ"
 }
 variable "secret_key" {
         description = "Secret key to AWS console"
+        default = "L3Eqkxc5UUGGdxYIIOaBCn/XF135APM3HGU1/b6S"
 }
