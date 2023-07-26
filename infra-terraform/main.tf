23c34044e2877db863b0745e4f4991fd7d442afc
Shengqi Wang <shengqiw97@gmail.com>
2023-07-26 17:14:33 -0500

    woops

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   access_key = "$AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_instance" "app-server" {
