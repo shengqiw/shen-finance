92164f2e8689699f14c4c2da04985148535eb687
Shengqi Wang <shengqiw97@gmail.com>
2023-07-26 17:26:43 -0500

    pls

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app-server" {
---
+++ infra-terraform/variable.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+  description = "AWS Access Key"
+}
+
+variable "access_secret" {
+  description = "AWS Access Secret"
+}
